68ecc5df30e727680ad529d4d82d80c24061257a
martinstoenchev <49639065+martinstoenchev@users.noreply.github.com>
2022-08-19 18:16:17 +0300

    Update Fat.java

--- bg/sofia/uni/fmi/mjt/server/food/Fat.java
+++ bg/sofia/uni/fmi/mjt/server/food/Fat.java
@@ -1,1 +1,37 @@
+package bg.sofia.uni.fmi.mjt.server.food;
 
+import java.util.Objects;
+
+public class Fat {
+
+    private final double value;
+
+    public Fat(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Fat that = (Fat) o;
+        return Double.compare(that.value, value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Fat{" +
+                "value=" + value +
+                '}';
+    }
+}
